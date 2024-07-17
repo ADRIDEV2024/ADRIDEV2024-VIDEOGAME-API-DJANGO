9d3408060daf93da3db22fd96ebc9fe03f2a1eb2
Adrian <adriangomis34@gmail.com>
2024-07-17 10:41:45 +0200

    Update Models.py

--- Models.py
+++ Models.py
@@ -4,17 +4,11 @@ from django.db import models
 class Videogame(models.Model):
     
     title = models.CharField(max_length=100, blank=False, default=None)
-    
     game_studio = models.CharField(max_length=100, blank=False, default=None)
-    
     creator = models.CharField(max_length=50, blank=False, default=None)
-    
     genre = models.CharField(max_length=50, default=None)
-    
     description = models.TextField(max_length=400,blank=False, default=None)
-    
     published_date = models.DateTimeField(default=None, auto_now_add=True)
-    
     steam_price = models.FloatField(max_length=50, default=None)
     
     
