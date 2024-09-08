66c1ea6d9d795b4abbd860546b46ea7c8499e03c
Adrian <adriangomis34@gmail.com>
2024-09-08 16:11:42 +0200

    Update Models.py

--- Models.py
+++ Models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Videogame(models.Model):
     
-    title = models.CharField(max_length=100, blank=True, default=None)
-    game_studio = models.CharField(max_length=100, blank=True, default=None)
+    title = models.CharField(max_length=100, blank=False, default=None)
+    game_studio = models.CharField(max_length=100, blank=False, default=None)
     creator = models.CharField(max_length=50, blank=False, default=None)
     genre = models.CharField(max_length=50, default=None)
     description = models.TextField(max_length=400,blank=False, default=None)
